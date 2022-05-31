d469bf31db4e3b1e491563c841e04f22aa0ddbf7
Alexandre Dulaunoy <a@foo.be>
2022-05-31 23:14:22 +0200

    chg: [tools] remove old Python 2

--- pybloom/utils.py
+++ pybloom/utils.py
@@ -9,16 +9,8 @@ running_python_3 = sys.version_info[0] == 3
 
 
 def range_fn(*args):
-    if running_python_3:
         return range(*args)
-    else:
-        return xrange(*args)
 
 
 def is_string_io(instance):
-    if running_python_3:
        return isinstance(instance, BytesIO)
-    else:
-        return isinstance(instance, (StringIO.StringIO,
-                                     cStringIO.InputType,
-                                     cStringIO.OutputType))
